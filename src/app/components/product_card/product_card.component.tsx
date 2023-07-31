1a2590012b59e429cbca4eacb9ee87840eb1c166
rashadNEON <rashad@neon.dev>
2023-07-31 08:45:55 +0200

    props fix in product card component

--- src/app/components/product_card/product_card.component.tsx
+++ src/app/components/product_card/product_card.component.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const ProductCard = (props) => {
-    return (
-        <div className="product-card">
-            <img src={props.img} alt="Product" />
-            <h3>{props.title}</h3>
-            <p>{props.size}</p>
-        </div>
-    );
+// @DanielBNEON
+// in typescript, variables can not have no type, so we need to create an interface
+// to define the type of the props
+// this interface is used in the function parameters
+
+interface ProductCardProps {
+  img: string;
+  title: string;
+  size: string;
+}
+
+const ProductCard = (props: ProductCardProps) => {
+  return (
+    <div className="product-card">
+      <img src={props.img} alt="Product" />
+      <h3>{props.title}</h3>
+      <p>{props.size}</p>
+    </div>
+  );
 };
 
 export default ProductCard;
